dd69949e2bfb4b372c6f152fc853bd2206e66ab7
Franco Odone <franco.odone.p@gmail.com>
2022-09-12 22:30:19 -0300

    Octavo commit avances javascript-4/css

--- script/script.js
+++ script/script.js
@@ -1,100 +1,76 @@
+// Variables fuera de funciones
 
+let selectedRow = null;
+let tareas = [];
 
-// EventListeners
-
-// document.querySelector('form').addEventListener('submit', onSubmitForm);
-
-document.querySelector('form').addEventListener('submit', (e) => {
-    
-    e.preventDefault();
-    onSubmitForm();
-    document.querySelector('form').reset();
-    // se vuelve a definir la variable que selecciona la fila deseada como null para que se pueda volver a usar 
-    selectedRow = null;
-});
-
-// Variables globales
-
-var selectedRow = null;
-var formData;
-var tareas = [];
-
-// Para 'evitar' que se eliminen los datos de la tabla de datos al hacer refresh
-
+// Para 'evitar' que se eliminen los datos de la tabla de datos al hacer refresh.
 updateAfterPageRefresh();
 
 // Funciones
 
 function onSubmitForm() {
 
-    formData = readForm();
-    if (selectedRow == null) {
+    let data = readForm();
+    if (selectedRow === null) {
 
-        insertNewRecord(formData);
+        insertNewRecord(data);
     } else {
 
-        updateRecord(formData);
+        updateRecord(data);
     };
 };
 
 function readForm() {
-    // Se construye el objeto
-    formData = {};
+    // Se construye el objeto.
+    const formData = {};
+
     formData['nombre'] = document.getElementById('nombre').value;
     formData['apellido'] = document.getElementById('apellido').value;
     formData['tarea'] = document.getElementById('tarea').value;
     formData['descripcion'] = document.getElementById('descripcion').value;
 
     return formData;
-
 };
 
-function insertNewRecord(formData) {
+function insertNewRecord(data) {
+    // table va a ser igual a la etiqueta tbody
+    let table = document.getElementById('listaTareas').getElementsByTagName('tbody')[0];
+    let newRow = table.insertRow();
 
-    var table = document.getElementById('listaTareas').getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow();
+    newRow.insertCell(0).innerHTML = data.nombre;
+    newRow.insertCell(1).innerHTML = data.apellido;
+    newRow.insertCell(2).innerHTML = data.tarea;
+    newRow.insertCell(3).innerHTML = data.descripcion;
 
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = formData.nombre;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = formData.apellido;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = formData.tarea;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = formData.descripcion;
+    // Se insertan los elementos que van a ser los botones de Edit y Delete, con sus respectivos eventos.
+    newRow.insertCell(4).innerHTML = `<a onClick=editForm(this)>Edit</a><a onClick=deleteRecord(this)>Delete</a>`;
 
-    // Se insertan los elementos que van a ser los botones de Edit y Delete, con sus respectivos eventos
+    // Para agregar los datos de los inputs (data) al array tareas.
+    tareas.push(data)
 
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = `<a onClick=editForm(this)>Edit</a><a onClick=deleteRecord(this)>Delete</a>`;
-
-    // para agregar los datos de los inputs (formData) al array tareas
-    tareas.push(formData)
-    // se vuelve el array un string para que se guarde asi en el localStorage
+    // Se vuelve el array un string para que se guarde asi en el localStorage.
     localStorage.setItem('dataTareas', JSON.stringify(tareas));
 };
 
 function deleteRecord(a) {
 
-    // son dos parentElement porque con el primero se obtiene el padre de <a></a> que es un <td></td>, y con el otro el padre de este que es el tr que necesito
+    // Son dos parentElement porque con el primero se obtiene el padre de <a></a> que es un <td></td>, y con el otro el padre de este que es el tr que necesito.
     let row = a.parentElement.parentElement;
 
-    let x = confirm('Confirme si desea eliminar este elemento');
-    
-    if (x == true) {
-        
-        // rowIndex entrega el indice del tr que se quiere eliminar 
-        tareas.splice(row.rowIndex - 1, 1);
-        // console.log(row.rowIndex);
+    let x = confirm('Confirme si desea eliminar esta tarea');
+
+    if (x === true) {
+
+        // rowIndex entrega el indice del tr que se quiere eliminar.        
+        tareas.splice((row.rowIndex - 1), 1);
+
         localStorage.setItem('dataTareas', JSON.stringify(tareas));
         document.getElementById('listaTareas').deleteRow(row.rowIndex);
-        // console.log(row);
     };
 };
 
 function editForm(a) {
 
-    // son dos parentElement porque con el primero se obtiene el padre de <a></a> que es un <td></td>, y con el otro el padre de este que es el tr que necesito
     selectedRow = a.parentElement.parentElement;
 
     document.getElementById('nombre').value = selectedRow.cells[0].innerHTML;
@@ -103,44 +79,54 @@ function editForm(a) {
     document.getElementById('descripcion').value = selectedRow.cells[3].innerHTML;
 }
 
-function updateRecord(formData) {
+function updateRecord(data) {
 
-    selectedRow.cells[0].innerHTML = formData.nombre;
-    selectedRow.cells[1].innerHTML = formData.apellido;
-    selectedRow.cells[2].innerHTML = formData.tarea;
-    selectedRow.cells[3].innerHTML = formData.descripcion;
+    selectedRow.cells[0].innerHTML = data.nombre;
+    selectedRow.cells[1].innerHTML = data.apellido;
+    selectedRow.cells[2].innerHTML = data.tarea;
+    selectedRow.cells[3].innerHTML = data.descripcion;
 
-    tareas.splice(selectedRow.rowIndex -1, 1, {nombre: formData.nombre, apellido: formData.apellido, tarea: formData.tarea, descripcion: formData.descripcion});
+    tareas.splice((selectedRow.rowIndex - 1), 1, {nombre: data.nombre, apellido: data.apellido, tarea: data.tarea, descripcion: data.descripcion });
     localStorage.setItem('dataTareas', JSON.stringify(tareas));
 }
 
 function updateAfterPageRefresh() {
 
-    if (localStorage.getItem('dataTareas') == null) {
+    if (localStorage.getItem('dataTareas') === null) {
 
-        console.log('No hay nada en el local storage');
+        void(0);
     } else {
-
-         // al obtener de vuelta el value del key, se parsea para que deje de ser un string y vuelva a ser un array
+        // Al obtener de vuelta el value del key, se parsea para que deje de ser un string y vuelva a ser un array.
         tareas = JSON.parse(localStorage.getItem('dataTareas'));
-        for (let index = 0; index < tareas.length; index++) {
-            
-           let n = tareas[index].nombre;
-           let a = tareas[index].apellido;
-           let t = tareas[index].tarea;
-           let d = tareas[index].descripcion; 
 
-           document.querySelector('#tbody').innerHTML += 
-           `<tr>
+        for (let index = 0; index < tareas.length; index++) {
+
+            let n = tareas[index].nombre;
+            let a = tareas[index].apellido;
+            let t = tareas[index].tarea;
+            let d = tareas[index].descripcion;
+
+            document.querySelector('#tbody').innerHTML +=
+            `<tr>
                 <td>${n}</td>
                 <td>${a}</td>
                 <td>${t}</td>
                 <td>${d}</td>
                 <td><a onClick=editForm(this)>Edit</a><a onClick=deleteRecord(this)>Delete</a></td>
-           </tr>
-           `
+            </tr>
+            `
         };
     };
 }
 
+// EventListeners
 
+document.querySelector('form').addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    onSubmitForm();
+    document.querySelector('form').reset();
+
+    // Se vuelve a definir la variable que selecciona la fila deseada como null para que se pueda volver a usar.
+    selectedRow = null;
+});
